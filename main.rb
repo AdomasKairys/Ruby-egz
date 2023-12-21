2fd335b6cf47a7d69650dc018f2d17a9950d05f9
AdomasKairys <adomas.kairys@ktu.edu>
2023-12-21 18:38:18 +0200

    slower with more ractors for some reason idk

--- main.rb
+++ main.rb
@@ -6,31 +6,71 @@ require './Utils.rb'
 lifters = Utils.read_json('Data/IF11_KairysA_LD1_dat2.json')
 lifters.each{ |l| Ractor.make_shareable l.freeze}
 
-NUM = 5
-
-distributor = Ractor.new do
-    loop do
-        Ractor.yield Ractor.receive
-    end
-end
+NUM = 4
+N = 100_000
 
 worker = NUM.times.map{
-    Ractor.new distributor do |dist|
-        while l = dist.take.dup
-            l.generate_hash
+    Ractor.new do
+        while l = Ractor.receive.dup
+            N.times.each{
+                l.generate_hash
+            }
             Ractor.yield l
         end
     end
 }
 
-(0..10).each{ |i|
-    distributor << lifters[i]
+result = Ractor.new do
+    r=[]
+    while l = Ractor.receive
+        r << l
+    end
+    Ractor.yield r
+end
+
+distributor = Ractor.new worker, result do |work, res|
+    w = nil
+    l = nil
+
+    for index in 0..NUM-1
+        l = Ractor.receive
+        work[index] << l
+    end
+
+    loop do
+        if w
+            l = Ractor.receive
+            w << l
+        end
+
+        if l == nil
+            work.delete w
+            break
+        end
+        w, resul = Ractor.select *work
+        print w
+        print "\n"
+        res << resul
+    end
+
+    work.each{
+        _r, resul = Ractor.select *work
+        res << resul
+    }
+    res << nil
+
+    Ractor.yield res.take
+end
+
+
+starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+lifters.each{ |l|
+    distributor << l
 }
-
-p (0..10).map{
-    _r, l = Ractor.select(*worker)
-    l
-}
-
-
-print p
+distributor << nil
+p = distributor.take
+ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
+elapsed = ending - starting
+print elapsed.to_s() +"\n"
+#print p
+print "\n"
