7e57adea17ac14c4844982aae868bf05c1ec3d38
AdomasKairys <adomas.kairys@ktu.edu>
2023-12-28 15:31:35 +0200

    Finished, changed code a bit added comments

--- main.rb
+++ main.rb
@@ -4,17 +4,21 @@ require './Lifter.rb'
 require './Utils.rb'
 require 'digest/sha2'
 
+=begin
+proccessor: Intel core i3-12100F, 4 cores 3.3 GHz up to 4.3 GHz, 8 threads
+single worker with N = 100_000
+time: 8.967 s
+with 4 workers with N = 100_000
+time: 4.896 s
+=end
+NUM_WOR = 4
+N = 100_000
+
 lifters = Utils.read_json('Data/IF11_KairysA_LD1_dat2.json')
-lifters.each{ |l| Ractor.make_shareable l.freeze}
-lifters.freeze
-sha256 = Digest::SHA256
-Ractor.make_shareable(sha256.freeze)
+Ractor.make_shareable(lifters) #important so that when sending it sends the same object and not a deep copy
 
-NUM = 4
-N = 1_000
-
-worker = NUM.times.map{
-    Ractor.new sha256 do |sh|
+worker = NUM_WOR.times.map{
+    Ractor.new Digest::SHA256 do |sh|
         hash = ""
         while l = Ractor.receive
             string = l.name + l.weight_class.to_s + '%.6f'%l.total.to_s #.6 because that was default with c++
@@ -22,10 +26,10 @@ worker = NUM.times.map{
             N.times do
                 hash = Utils.generate_hash(sh, string)
             end
-            if hash[0].match /[0-9]/
-                Ractor.yield nil
-            else
+            unless hash[0].match /[0-9]/
                 Ractor.yield [l,hash]
+            else
+                Ractor.yield nil
             end
         end
     end
@@ -34,8 +38,10 @@ worker = NUM.times.map{
 result = Ractor.new do
     hashed_lifters=[]
     while l = Ractor.receive
-        insert_at = hashed_lifters.bsearch_index { |hl| hl[0].weight_class <= l[0].weight_class }
-        if insert_at
+        insert_at = hashed_lifters.bsearch_index { |hl|
+            hl[0].weight_class < l[0].weight_class || (hl[0].weight_class == l[0].weight_class && hl[0].total <= l[0].total)
+        }
+        if insert_at #bsearch_index returns nil if the obj needs to be placed at the end
             hashed_lifters.insert(insert_at, l)
             next
         end
@@ -65,25 +71,23 @@ logger = Ractor.new do
 end
 
 distributor = Ractor.new worker, result, logger, output do |work, res, log, out|
-    w = nil
     l = nil
+    is_finished = false
+
+    #pre populating workers
     work.each{ |wr|
         l = Ractor.receive
         log << "Recevied form main"
         wr << l
         log << "Sent to worker " + wr.to_s
     }
-    loop do
-        if w
+    until work.empty?
+        unless is_finished
             l = Ractor.receive
             log << "Recevied form main"
-            w << l
-            log << "Sent to worker " + w.to_s
+            is_finished = l == nil
         end
-        if l == nil
-            log << "Signal for no more data from main"
-            break
-        end
+
         w, resul = Ractor.select *work
         log << "Took from worker " + w.to_s
         if resul
@@ -92,15 +96,15 @@ distributor = Ractor.new worker, result, logger, output do |work, res, log, out|
         else
             log << "Element filtered out by worker " + w.to_s
         end
+
+        if is_finished
+            work.delete w
+        else
+            w << l
+            log << "Sent to worker " + w.to_s
+        end
     end
-    work.delete w
-    work.each{
-        _r, resul = Ractor.select *work
-        log << "Took from worker " + _r.to_s
-        work.delete _r
-        res << resul
-        log << "Sent to results " + res.to_s
-    }
+
     res << nil
     log << "Sent signal for no more data to results " + res.to_s
     out << res.take
@@ -111,15 +115,19 @@ distributor = Ractor.new worker, result, logger, output do |work, res, log, out|
     Ractor.yield true
 end
 
+if lifters.size < NUM_WOR
+    raise "File can't have less entries than workers"
+end
+
 starting = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 
 lifters.each{ |l|
     distributor << l
 }
-
 distributor << nil
 distributor.take
 
 ending = Process.clock_gettime(Process::CLOCK_MONOTONIC)
 elapsed = ending - starting
+
 print elapsed.to_s() +"\n"
